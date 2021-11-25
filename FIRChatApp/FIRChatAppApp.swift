c0bbc5766f78ba5fd599c8c584d33cff5f87f398
Trung Hoang <hoang.van.trung@mirabo.com.vn>
2021-11-25 22:37:23 +0700

    Initial Commit

---
+++ FIRChatApp/FIRChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FIRChatAppApp.swift
+//  FIRChatApp
+//
+//  Created by Trung Hoang on 25/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct FIRChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
